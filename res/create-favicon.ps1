b827155e2a31b7e18ccf6513d3ae9d373c050b4c
Jan Kuehle <jkuehle90@gmail.com>
2020-07-28 22:33:34 +0100

    Add web app manifest and icons

--- res/create-favicon.ps1
+++ res/create-favicon.ps1
@@ -1,6 +1,6 @@
 function CreatePNG([int]$DPI, [int]$Size) {
 	& inkscape `
-		--export-filename="$PSScriptRoot/favicon-$Size.png" `
+		--export-filename="$PSScriptRoot/../src/images/logo-$Size.png" `
 		--export-type=png `
 		--export-area-page `
 		--export-dpi=$DPI `
@@ -9,11 +9,21 @@ function CreatePNG([int]$DPI, [int]$Size) {
 
 CreatePNG 48 16
 CreatePNG 96 32
+CreatePNG 144 48
+CreatePNG 216 72
+CreatePNG 288 96
+CreatePNG 432 144
+CreatePNG 504 168
+CreatePNG 576 192
 
 imconvert `
-	"$PSScriptRoot/favicon-16.png" `
-	"$PSScriptRoot/favicon-32.png" `
+	"$PSScriptRoot/../src/images/logo-16.png" `
+	"$PSScriptRoot/../src/images/logo-32.png" `
 	"$PSScriptRoot/../src/favicon.ico"
 
-rm "$PSScriptRoot/favicon-16.png"
-rm "$PSScriptRoot/favicon-32.png"
+imconvert `
+	"$PSScriptRoot/../src/images/logo-192.png" `
+	-background white `
+	-alpha remove `
+	-alpha off `
+	"$PSScriptRoot/../src/images/logo-192-opaque.png"
