c6d29c8f21f432e443ca5a1143011922c03b5e2c
Jan Kuehle <jkuehle90@gmail.com>
2020-07-28 23:23:58 +0100

    Force new SW to take over

--- src/sw.js
+++ src/sw.js
@@ -17,6 +17,7 @@ const CACHE_URLS = [
 ];
 
 addEventListener("install", (event) => {
+	self.skipWaiting();
 	event.waitUntil(
 		caches.open(CACHE_NAME).then((cache) => cache.addAll(CACHE_URLS))
 	);
