ee705bbe04c4349d3aa8b90cd57b10d577db7318
Jan Kuehle <jkuehle90@gmail.com>
2021-01-17 13:13:07 +0100

    Switch to GKE

--- deploy/deploy.ps1
+++ deploy/deploy.ps1
@@ -1,3 +1,3 @@
 docker build -t frigus02/chooser .
 docker push frigus02/chooser
-kubectl --context kubesail-frigus02 delete pod -l app=chooser
+kubectl --context gke_me-kuehle_europe-west3-a_website delete pod -l app=chooser
